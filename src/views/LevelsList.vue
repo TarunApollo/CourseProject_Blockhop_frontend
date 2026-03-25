8ba77a01780fa5d094ece3951a65dfd358eafc37
Theodoros Thomas <theodoros.thomas@usi.ch>
2026-03-25 19:08:58 +0100

    added the background and back button in levellist view

--- src/views/LevelsList.vue
+++ src/views/LevelsList.vue
@@ -1,8 +1,25 @@
 <script setup>
 import LevelListElement from "../components/LevelListElement.vue"
 import tempLevelsList from "../stores/tempLevelsStore"
+import GameBackground from "../components/GameBackground.vue"
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+const goBack = () => {
+  if (window.history.length > 1) {
+    router.back()
+    return
+  }
+
+  router.push('/home')
+}
 </script>
 <template>
+    <!-- <GameBackground /> -->
+    <div class="return-button">
+        <button class="menu-button" type="button" @click="goBack">&#8592;</button>
+    </div>
     <header class="sorting-header">
         Sorting by:
         <div class="inline-dropdown"> Hello
@@ -80,4 +97,34 @@ import tempLevelsList from "../stores/tempLevelsStore"
         display: block;
     }
 
+    .return-button {
+        pointer-events: auto;
+        position: absolute;
+        top: 28px;
+        left: 28px;
+    }
+
+    .menu-button {
+        padding: clamp(8px, 2vw, 8px) clamp(15px, 5vw, 15px);
+        background: #4ade80;
+        color: #052e16;
+        border: #181818 2px solid;
+        font-family: 'Pixelify Sans', monospace;
+        font-size: clamp(0.2rem, 3.4vw, 4rem);
+        font-weight: 700;
+        cursor: pointer;
+        white-space: nowrap;
+        box-shadow: 0 5px 0 #166534, 0 8px 18px rgba(0,0,0,0.25);
+        transition: transform 0.07s, box-shadow 0.07s;
+        text-align: center;
+    }
+
+    .menu-button:hover {
+        transform: translateY(-2px);
+        background: #86efac;
+    }
+
+    .menu-button:active {
+        transform: translateY(0);
+    }
 </style>
