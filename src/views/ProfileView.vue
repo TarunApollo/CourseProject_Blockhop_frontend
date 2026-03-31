ec95fa4fb60092df50b9364dfc86b17b558a2fca
Theodoros Thomas <theodoros.thomas@usi.ch>
2026-03-31 17:34:17 +0200

    modified profile view design to make it consistent with the rest

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -5,9 +5,8 @@ import ProfileHeaderPanel from '@/features/profile/component/ProfileHeaderPanel.
 import PlayedLevelsPanel from '@/features/profile/component/PlayedLevelsPanel.vue'
 import CompletedLevelsPanel from '@/features/profile/component/CompletedLevelsPanel.vue'
 import CreatedLevelsPanel from '@/features/profile/component/CreatedLevelsPanel.vue'
-import BackButton from '@/components/BackButton.vue'
-import Sun from '@/components/Sun.vue'
-import BlockhopWordmark from '@/components/BlockhopWordmark.vue'
+import ReturnButton from '@/shared/components/ReturnButton.vue'
+import MainLogo from '@/shared/components/MainLogo.vue'
 import { gameVisualTokens } from '@/shared/lib/visualizationTokens'
 
 const profileTokens = gameVisualTokens
@@ -22,55 +21,39 @@ onMounted(() => {
 
 
 <template>
-  <section :class="[profileTokens.backgrounds.sky, 'relative min-h-[calc(100vh-2rem)] w-full overflow-hidden']">
-    <div class="absolute left-4 top-4 z-20 sm:left-6 sm:top-5">
-      <BackButton />
-    </div>
+    <ReturnButton />
 
-    <div class="absolute right-4 top-4 z-20 sm:right-6 sm:top-5">
-      <Sun />
-    </div>
+    <div class="relative flex min-h-screen flex-col">
+      <MainLogo />
 
-    <div class="relative z-10 min-h-[calc(100vh-2rem)] px-4 py-6 sm:px-6 lg:px-8">
-      <div class="mx-auto flex w-full max-w-none flex-col items-center">
-        <div class="pb-8 pt-4 text-center sm:pb-12 sm:pt-2 lg:pb-16">
-          <BlockhopWordmark />
-        </div>
+      <div class="flex flex-1 justify-center px-4 pb-6 sm:px-6 lg:px-8">
+        <div class="w-full max-w-[600px] sm:max-w-[640px] lg:max-w-[720px]">
+          <div
+              v-if="loading"
+              :class="[profileTokens.backgrounds.primaryPanel, 'w-full px-6 py-10']"
+          >
+            <p :class="[profileTokens.text.primary, 'text-center text-2xl']">Loading profile...</p>
+          </div>
 
-        <div class="flex w-full justify-center">
+          <div
+              v-else-if="error"
+              :class="[profileTokens.backgrounds.primaryPanel, 'w-full px-6 py-10']"
+          >
+            <p :class="[profileTokens.text.primary, 'text-center text-2xl']">
+              Failed to load profile data.
+            </p>
+          </div>
 
-          <div class="w-full max-w-[600px] sm:max-w-[640px] lg:max-w-[720px]">
-            <div
-                v-if="loading"
-                :class="[profileTokens.backgrounds.primaryPanel, 'w-full px-6 py-10']"
-            >
-              <p :class="[profileTokens.text.primary, 'text-center text-2xl']">Loading profile...</p>
-            </div>
+          <div v-else class="flex w-full flex-col gap-8 sm:gap-10">
+            <ProfileHeaderPanel :username="username" />
 
-            <div
-                v-else-if="error"
-                :class="[profileTokens.backgrounds.primaryPanel, 'w-full px-6 py-10']"
-            >
-              <p :class="[profileTokens.text.primary, 'text-center text-2xl']">
-                Failed to load profile data.
-              </p>
-            </div>
+            <PlayedLevelsPanel :levels-played="levelsPlayed" />
 
-            <div v-else class="flex w-full flex-col gap-8 sm:gap-10">
-              <ProfileHeaderPanel :username="username" />
+            <CompletedLevelsPanel :levels-completed="levelsCompleted" />
 
-              <PlayedLevelsPanel :levels-played="levelsPlayed" />
-
-              <CompletedLevelsPanel :levels-completed="levelsCompleted" />
-
-              <CreatedLevelsPanel :created-levels="createdLevels" />
-            </div>
-
+            <CreatedLevelsPanel :created-levels="createdLevels" />
           </div>
         </div>
-
       </div>
     </div>
-
-  </section>
 </template>
