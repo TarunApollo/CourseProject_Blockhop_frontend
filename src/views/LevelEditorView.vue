d8c0f0cffe7d58c709993da29251efc265f0b586
Kevin Johansson <kevin.johansson@usi.ch>
2026-04-04 23:20:31 +0200

    Added /editor route and the main editor view

---
+++ src/views/LevelEditorView.vue
@@ -0,0 +1,12 @@
+<script setup>
+import BackButton from '@/components/BackButton.vue'
+</script>
+
+<template>
+  <div class="level-editor h-screen flex flex-col bg-sky-200">
+    <header class="flex items-center p-4 gap-4 bg-[#7BCF73] border-b-2 border-[#5A7E4B]">
+      <BackButton />
+      <h1 class="text-2xl font-bold text-[#1F3B17]">Level Editor</h1>
+    </header>
+  </div>
+</template>
