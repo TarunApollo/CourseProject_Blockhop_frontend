dccef2cb997ad37094bed672a0d2c414fa173b10
Kevin Johansson <kevin.johansson@usi.ch>
2026-04-05 00:46:48 +0200

    made tiles selectable in the tile picker

--- src/features/level-editor/components/TileSidebar.vue
+++ src/features/level-editor/components/TileSidebar.vue
@@ -4,7 +4,7 @@ import { useEditorState } from '../composables/useEditorState'
 import { groundTiles, objectTiles } from '../lib/tileData'
 import TileSelector from './TileSelector.vue'
 
-const { activeLayer } = useEditorState()
+const { activeLayer, selectedTile, setSelectedTile } = useEditorState()
 
 const tilesToShow = computed(() => {
   return activeLayer.value === 'ground' ? groundTiles : objectTiles
@@ -49,6 +49,8 @@ const categoryLabels = {
             v-for="tile in tiles"
             :key="tile.gid"
             :tile="tile"
+            :selected="selectedTile?.gid === tile.gid"
+            @click="setSelectedTile(tile)"
           />
         </div>
       </div>
--- src/features/level-editor/composables/useEditorState.js
+++ src/features/level-editor/composables/useEditorState.js
@@ -1,14 +1,21 @@
 import { ref } from 'vue'
 
 const activeLayer = ref('ground')
+const selectedTile = ref(null)
 
 export function useEditorState() {
   function setActiveLayer(layer) {
     activeLayer.value = layer
   }
 
+  function setSelectedTile(tile) {
+    selectedTile.value = tile
+  }
+
   return {
     activeLayer,
-    setActiveLayer
+    selectedTile,
+    setActiveLayer,
+    setSelectedTile
   }
 }
