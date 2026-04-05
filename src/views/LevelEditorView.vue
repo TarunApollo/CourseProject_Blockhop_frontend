ffc52778690f0795542d4f5beed31e924ef9d968
Kevin Johansson <kevin.johansson@usi.ch>
2026-04-05 20:00:00 +0200

    Removed padding from /editor. Should be applied universally imo.

--- src/views/LevelEditorView.vue
+++ src/views/LevelEditorView.vue
@@ -6,15 +6,15 @@ import TileSidebar from '@/features/level-editor/components/TileSidebar.vue'
 </script>
 
 <template>
-  <div class="level-editor h-screen flex flex-col bg-sky-200">
-    <header class="flex items-center p-4 gap-4 bg-editor-bg border-b-2 border-editor-border">
+  <div class="level-editor h-screen w-screen flex flex-col overflow-hidden">
+    <header class="flex items-center px-4 py-2 bg-editor-bg border-b-2 border-editor-border shrink-0">
       <BackButton />
-      <h1 class="text-2xl font-bold text-editor-text">Level Editor</h1>
+      <h1 class="ml-4 text-xl font-bold text-editor-text" style="padding-left: 15px;">Level Editor</h1>
     </header>
 
     <EditorToolbar />
 
-    <main class="flex-1 flex overflow-hidden">
+    <main class="flex-1 flex overflow-hidden min-h-0">
       <EditorCanvas class="flex-1 min-w-0" />
       <TileSidebar class="shrink-0" />
     </main>
