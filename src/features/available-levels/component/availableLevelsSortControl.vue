15b93124027cba64b581fd6fc231db7c6db7b975
Chengyu Yu <nightnight2024@gmail.com>
2026-04-07 20:18:04 +0200

    feat: add sorting control
    Co-authored-by: Salma Abu Husein <Salma.Abu.husein@usi.ch>

---
+++ src/features/available-levels/component/availableLevelsSortControl.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { PUBLISHED_LEVEL_SORT_OPTIONS } from '@/features/available-levels/lib/publishedLevelsContract.js'
+
+defineProps({
+  modelValue: {
+    type: String,
+    required: true,
+  },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+function handleChange(event) {
+  emit('update:modelValue', event.target.value)
+}
+</script>
+
+<template>
+  <label>
+    Sort by
+    <select :value="modelValue" @change="handleChange">
+      <option
+        v-for="option in PUBLISHED_LEVEL_SORT_OPTIONS"
+        :key="option"
+        :value="option"
+      >
+        {{ option }}
+      </option>
+    </select>
+  </label>
+</template>
