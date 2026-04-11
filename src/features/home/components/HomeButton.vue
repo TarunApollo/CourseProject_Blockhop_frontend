872b09e85d1b6f1a0bf19ae50963e043828c13af
Leon Jacopo Costa <leon.costa@usi.ch>
2026-04-11 13:44:03 +0200

    chore(ui): cleanup unrelated feature and removed unused svg files

--- src/features/home/components/HomeButton.vue
+++ src/features/home/components/HomeButton.vue
@@ -11,7 +11,7 @@ const props = defineProps({
 
 <template>
   <RouterLink
-    :to="to"
+    :to="props.to"
     class="ui-btn bg-dirt-block bg-home-btn-dirt-dark text-white relative flex flex-col items-center justify-center text-4xl font-bold py-6 px-4"
   >
     <span class="relative z-10 [text-shadow:3px_3px_0px_#000000]">
--- src/shared/components/ReturnButton.vue
+++ src/shared/components/ReturnButton.vue
@@ -1,12 +1,19 @@
 <script setup>
 import { RouterLink } from "vue-router";
 import BackArrow from "@/assets/BackArrow.vue";
+
+const props = defineProps({
+  to: {
+    type: String,
+    default: "/home",
+  },
+});
 </script>
 
 <template>
   <div class="absolute left-10 top-10 z-100">
     <RouterLink
-      to="/home"
+      :to="props.to"
       class="ui-btn relative flex items-center justify-center w-16 h-16"
     >
       <BackArrow />
