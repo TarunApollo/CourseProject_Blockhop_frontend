d782c0c43a9554a2de3e83205fdd0f6202136b2e
Kevin Johansson <kevin.johansson@usi.ch>
2026-04-16 00:41:20 +0200

    Removed the play button from /home

--- src/features/home/components/HomeMenu.vue
+++ src/features/home/components/HomeMenu.vue
@@ -6,7 +6,6 @@ import Button from "@/shared/components/Button.vue";
   <nav class="flex flex-col gap-[3vmin] w-[25vmin] justify-center">
     <Button to="/profile">Profile</Button>
     <Button to="/level-list">Levels</Button>
-    <Button to="/play">Play</Button>
     <Button to="/create-level">Create</Button>
     <Button to="/about">About</Button>
   </nav>
