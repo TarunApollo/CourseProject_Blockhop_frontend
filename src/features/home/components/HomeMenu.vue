8366c7803016b3acadb973b4f90c84ff3b40d927
Kevin Johansson <kevin.johansson@usi.ch>
2026-04-16 14:52:49 +0200

    Changed "Levels" to "Explore" on /home

--- src/features/home/components/HomeMenu.vue
+++ src/features/home/components/HomeMenu.vue
@@ -5,7 +5,7 @@ import Button from "@/shared/components/Button.vue";
 <template>
   <nav class="flex flex-col gap-[3vmin] w-[25vmin] justify-center">
     <Button to="/profile">Profile</Button>
-    <Button to="/level-list">Levels</Button>
+    <Button to="/level-list">Explore</Button>
     <Button to="/create-level">Create</Button>
     <Button to="/about">About</Button>
   </nav>
