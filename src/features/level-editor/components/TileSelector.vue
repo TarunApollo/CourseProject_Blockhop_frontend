9254c1b4fc20c4298814e98e624d1af2ed31d0e2
Tarun Deepak <tarundeepak2005@gmail.com>
2026-05-06 17:49:42 +0200

    fix : removed exclamation icon for masked door and flag after placed

--- src/features/level-editor/components/TileSelector.vue
+++ src/features/level-editor/components/TileSelector.vue
@@ -98,7 +98,7 @@ const displayName = computed(() => {
         : 'border-transparent hover:border-editor-border bg-white/50',
       disabled ? 'cursor-not-allowed opacity-80 hover:border-transparent' : ''
     ]"
-    :title="tile.type"
+    :title="disabledMessage || tile.type"
   >
     <div
       class="tile-preview rounded relative overflow-hidden"
@@ -133,37 +133,6 @@ const displayName = computed(() => {
       v-if="disabled"
       class="absolute inset-1 rounded-md bg-white/55 pointer-events-none"
     />
-    <div
-      v-if="disabled"
-      class="absolute top-0 right-0 z-10"
-      style="transform: translate(25%, -25%)"
-    >
-      <div class="relative group">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="#fbbf24"
-          stroke="#92400e"
-          stroke-width="1.5"
-          class="w-4 h-4"
-        >
-          <path
-            fill-rule="evenodd"
-            d="M9.401 3.003c1.155-2 4.043-2 5.197 0l7.355 12.748c1.154 2-.29 4.5-2.599 4.5H4.645c-2.309 0-3.752-2.5-2.598-4.5L9.4 3.003zM12 8.25a.75.75 0 01.75.75v3.75a.75.75 0 01-1.5 0V9a.75.75 0 01.75-.75zm0 8.25a.75.75 0 100-1.5.75.75 0 000 1.5z"
-            clip-rule="evenodd"
-          />
-        </svg>
-        <div
-          v-if="disabledMessage"
-          class="absolute bottom-full right-0 mb-1.5 hidden group-hover:block w-48 bg-[#1F3B17] text-white text-xs rounded-lg px-3 py-2 shadow-lg pointer-events-none whitespace-normal text-left"
-        >
-          {{ disabledMessage }}
-          <div class="absolute top-full right-2 -mt-px">
-            <div class="border-4 border-transparent border-t-[#1F3B17]"></div>
-          </div>
-        </div>
-      </div>
-    </div>
     <span class="text-xs text-editor-text mt-1 truncate w-full text-center">
       {{ displayName }}
     </span>
