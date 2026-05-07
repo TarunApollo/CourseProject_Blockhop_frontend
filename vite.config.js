80dd5e141bfc075f90423744e7305e0102bdbd17
Kevin Johansson <kevin.johansson@usi.ch>
2026-05-07 18:21:19 +0200

    Added js obfuscation and anti-debugging features via a vite plugin. Makes cheating substantially more tedious and harder.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite'
+import vitePluginBundleObfuscator from 'vite-plugin-bundle-obfuscator'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,10 +14,42 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
     tailwindcss(),
+    vitePluginBundleObfuscator({
+      enable: true,
+      autoExcludeNodeModules: true,
+      threadPool: true,
+      apply: 'build',
+      options: {
+        compact: true,
+        controlFlowFlattening: true,
+        controlFlowFlatteningThreshold: 0.75,
+        deadCodeInjection: true,
+        deadCodeInjectionThreshold: 0.4,
+        debugProtection: true,
+        disableConsoleOutput: false,
+        identifierNamesGenerator: 'hexadecimal',
+        selfDefending: true,
+        simplify: true,
+        stringArray: true,
+        stringArrayThreshold: 0.75,
+        stringArrayEncoding: ['base64'],
+        stringArrayCallsTransform: true,
+        stringArrayCallsTransformThreshold: 0.5,
+        stringArrayRotate: true,
+        stringArrayShuffle: true,
+        splitStrings: true,
+        splitStringsChunkLength: 5,
+        ignoreImports: true,
+        unicodeEscapeSequence: false,
+      },
+    }),
   ],
+  build: {
+    sourcemap: false,
+  },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
