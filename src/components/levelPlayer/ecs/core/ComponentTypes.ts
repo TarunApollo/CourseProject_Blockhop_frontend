833a2b4f74567ac51e6bbd9cc0b3df609956e6ef
Leon Jacopo Costa <leon.costa@usi.ch>
2026-05-15 21:11:09 +0200

    feat(engine): impl type safe getComponent, addComponent with white magic
    - removed EntityManager.ts since methods were not useful
    - type code goes inside new file ComponentMeta.ts
    - split enum for state into separate file
    - keep in mind this commit is broken because imports
    have not been changed across the engine code! (not done for brevity and
    scope)
    
    Co-authored-by: Valerie Burgener <burgev@usi.ch>

--- src/components/levelPlayer/ecs/core/ComponentTypes.ts
+++ src/components/levelPlayer/ecs/core/ComponentTypes.ts
@@ -1,47 +1,19 @@
-export const ComponentTypes = {
-  Transform: 1 << 0,
-  Sprite: 1 << 2,
-  Physics: 1 << 3,
-  Player: 1 << 4,
-
-  /**
-   * abstraction on movementSystem
-   * shell + snail + slime have similar logic in movement
-   * so abstract them in name horizontalWalker
-   *
-   * if deepseek wanna add bee add a flyer
-   */
-  HorizontalWalker: 1 << 5,
-
-  Door: 1 << 8,
-
-  /**
-   * hazard is a damage identity to abstract hazard in damage system
-   */
-  Hazard: 1 << 10,
-
-  /**
-   * enemy is a collision identity to abstract slime and snail
-   */
-  Enemy: 1 << 18,
-
-  /**
-   * component for normalize falling down the map
-   * for snail,slime and shell
-   */
-
-  OutOfBounds: 1 << 7,
-
-  /**
-   * component for dynamic collision filter of player
-   */
-  PlayerCollisionFilter: 1 << 9,
-
-  StartFlag: 1 << 11,
-  Animator: 1 << 12,
-  Slime: 1 << 13,
-  Shell: 1 << 14,
-  Snail: 1 << 15,
-  DestructibleBox: 1 << 16,
-  Coin: 1 << 17,
-};
+export const CT = {
+  Transform: 1,
+  Sprite: 2,
+  Physics: 4,
+  Player: 8,
+  HorizontalWalker: 16,
+  Door: 32,
+  OutOfBounds: 64,
+  PlayerCollisionFilter: 128,
+  Hazard: 256,
+  StartFlag: 512,
+  Animator: 1024,
+  Slime: 2048,
+  Shell: 4096,
+  Snail: 8192,
+  DestructibleBox: 16384,
+  Coin: 32768,
+  Enemy: 65536,
+} as const;
